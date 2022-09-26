d055a0e93bfe71780101b5850934949ce2056660
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-09-26 19:35:27 +0300

    [features] updated script and design

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ const bg = document.querySelector('.wrapper');
 button.addEventListener('click', changingBgWhite);
 
 function changingBgWhite(){
-    bg.classList.replace('wrapper', 'wrapper_white')
+    //bg.classList.replace('wrapper', 'wrapper_opacity')
+    bg.className = 'wrapper_opacity';
     button.removeEventListener('click', changingBgWhite)
     button.addEventListener('click', changingBgDark);
 }
 
 function changingBgDark(){
-    bg.classList.replace('wrapper_white', 'wrapper')
+    //bg.classList.replace('wrapper_opacity', 'wrapper')
+    bg.className = 'wrapper_back';
     button.removeEventListener('click', changingBgDark);
     button.addEventListener('click', changingBgWhite);
 }
